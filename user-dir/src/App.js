523533e46287b28e46e28f5f9ba3d40b0549ab39
Chris Sison <chris.a.sison@outlook.com>
2020-11-20 18:07:52 -0800

    init

---
+++ user-dir/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Table from "./components/Table/Table";
+import Jumbo from "./components/Jumbo/Jumbo";
+
+function App() {
+  return (
+    <div className="App">
+      <Jumbo />
+      <Table />
+    </div>
+  );
+}
+
+export default App;
---
+++ user-dir/src/components/Jumbo/Jumbo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Jumbo.css";
+
+function Jumbo() {
+    return (
+        <div className="jumbotron" id="jumbotron">
+            <h5 id="jumbo-header">User Directory</h5>
+            <hr />
+            <p className="lead">Track your users.</p>
+        </div>
+    )
+}
+
+export default Jumbo
